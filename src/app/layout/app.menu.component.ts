4d45b9134951eeb83d3950b503d45e60892a08d6
a-rRehman <70515831+a-rRehman@users.noreply.github.com>
2024-06-04 14:17:38 +0500

    Dashboard Updated

--- src/app/layout/app.menu.component.ts
+++ src/app/layout/app.menu.component.ts
@@ -24,19 +24,19 @@ export class AppMenuComponent implements OnInit {
                 ],
             },
             {
-                label: 'ORDER MANAGEMENT',
+                label: 'Order Managment',
                 items: [
                     {
-                        label: 'Submenu 1',
+                        label: 'Orders',
                         icon: 'pi pi-fw pi-bookmark',
                         items: [
                             {
-                                label: 'Submenu 1.1',
+                                label: 'Manage Orders',
                                 icon: 'pi pi-fw pi-bookmark',
                                 routerLink: ['/uikit/myForm'],
                             },
                             {
-                                label: 'Submenu 1.2',
+                                label: 'Browse Orders',
                                 icon: 'pi pi-fw pi-bookmark',
                                 routerLink: ['/uikit/treeTable'],
                             },
